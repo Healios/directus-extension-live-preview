b55ca90d9b086c50c1ef63ac0a28853e7030b753
Healios <starwop@gmail.com>
2024-10-03 21:30:17 +0200

    Latest changes. Still work in progress.

---
+++ src/helpers.ts
@@ -0,0 +1,252 @@
+import { AxiosInstance } from "axios";
+import { Relation, Field } from "@directus/types"
+
+export async function getRelations(api: AxiosInstance): Promise<Relation[]>
+{
+    const relationsResponse = await api.get("/relations");
+
+    if (relationsResponse.status !== 200)
+    {
+        console.error("[Live Preview] Unable to retrieve relations!");
+        return [];
+    }
+    
+    return relationsResponse.data.data as Relation[];
+}
+
+export function getRelationsForCollection(relations: Relation[], collection: string)
+{
+    return relations.filter((relation: Relation) => relation.collection === collection || relation.related_collection === collection);
+};
+
+export function getRelationsForField(relations: Relation[], fieldInfo: Field): Relation[]
+{
+    const foundRelations = relations.filter((relation: Relation) => (relation.collection === fieldInfo.collection && relation.field === fieldInfo.field) || (relation.related_collection === fieldInfo.collection && relation.meta?.one_field === fieldInfo.field));
+
+    if (foundRelations.length > 0)
+    {
+        const isM2M = foundRelations[0]!.meta?.junction_field !== null;
+
+        // If the relation matching the field has a junction field, it's a m2m. In that case,
+        // we also want to return the secondary relationship (from the jt to the related)
+        // so any ui elements (interfaces) can utilize the full relationship
+        if (isM2M)
+        {
+            const secondaryRelation = relations.find((relation) => relation.collection === foundRelations[0]!.collection && relation.field === foundRelations[0]!.meta?.junction_field);
+            if (secondaryRelation) foundRelations.push(secondaryRelation);
+        }
+    }
+
+    return foundRelations;
+};
+
+export function getRelationForField(relations: Relation[], fieldInfo: Field): Relation | null
+{
+    const foundRelations = relations.filter((relation: Relation) => (relation.collection === fieldInfo.collection && relation.field === fieldInfo.field) || (relation.related_collection === fieldInfo.collection && relation.meta?.one_field === fieldInfo.field));
+    return foundRelations.find((relation) => relation.collection === fieldInfo.collection && relation.field === fieldInfo.field) || null;
+};
+
+export async function updateRelatedItemsRecursively(api: AxiosInstance, relations: Relation[], collection: string, item: any, oldFormValues: any, newFormValues: any)
+{
+    for(const field of Object.keys(item))
+    {
+        // Get more information about the current field.
+        const fieldTypeResponse = await api.get(`/fields/${collection}`);
+        if (fieldTypeResponse.status !== 200)
+        {
+            console.error(`[Live Preview] Unable to retrieve meta information about field ${field}`);
+            return "";
+        }
+        const fieldInfo = fieldTypeResponse.data.data.find((fieldInfo: Field) => fieldInfo.field === field) as Field;
+
+        switch (true)
+        {
+            case fieldInfo.meta?.special?.includes("m2o"):
+                await handleM2O(api, relations, item, oldFormValues, newFormValues, fieldInfo);
+                break;
+
+            case fieldInfo.meta?.special?.includes("o2m"):
+                await handleO2M(api, relations, item, oldFormValues, newFormValues, fieldInfo);
+                break;
+
+            case fieldInfo.meta?.special?.includes("m2m"):
+                break;
+
+            case fieldInfo.meta?.special?.includes("m2a"):
+                await handleM2A(api, item, newFormValues, fieldInfo);
+                break;
+                
+            // When the field isn't a relation type field, simply override the value.
+            default:
+                item[field] = newFormValues[field];
+                break;
+        }
+    }
+
+    return;
+};
+
+async function handleM2O(api: AxiosInstance, relations: Relation[], item: any, oldFormValues: any, newFormValues: any, fieldInfo: Field)
+{
+    // When the new value is null, simply set the value.
+    if (newFormValues[fieldInfo.field] === null)
+    {
+        item[fieldInfo.field] = newFormValues[fieldInfo.field];
+        return;
+    }
+
+    // Skip, if there are no changes or if the new value is null.
+    if (oldFormValues[fieldInfo.field] === newFormValues[fieldInfo.field]) return;
+
+    // When a new item is created, but hasn't been saved, an object with the new values will be returned, instead of a string id.
+    if (Object.prototype.toString.call(newFormValues[fieldInfo.field]) === "[object Object]")
+    {
+        item[fieldInfo.field] = newFormValues[fieldInfo.field];
+        return;
+    }
+
+    // Get the items foreign collection.
+    const foreignCollection = fieldInfo.schema!.foreign_key_table;
+
+    // Get new item value from foreign collection.
+    const response = await api.get(`/items/${foreignCollection}/${newFormValues[fieldInfo.field]}?fields[]=*.*.*.*.*`);
+    if (response.status !== 200)
+    {
+        console.error(`[Live Preview] Unable to retrieve information about item with id ${newFormValues[fieldInfo.field]} in foreign collection ${foreignCollection}`);
+        return "";
+    }
+
+    // Update the item value (there will most likely be more properties than originally, but that's ok, as it's all in a private preview).
+    item[fieldInfo.field] = { ...response.data.data };
+    return;
+};
+
+async function handleO2M(api: AxiosInstance, relations: Relation[], item: any, oldFormValues: any, newFormValues: any, fieldInfo: Field)
+{
+    // Get changes.
+    const changes = newFormValues[fieldInfo.field];
+
+    // When an item is added, but not saved, and then removed, the value received from Directus is undefined for some reason.
+    // In this case, we can fix it by finding the saved items and thereby resetting the value.
+    if (changes === undefined)
+    {
+        item[fieldInfo.field] = item[fieldInfo.field].filter((tempItem: any) => tempItem.id !== undefined);
+        return;
+    }
+
+    // Handle additions.
+    const additions = changes["create"];
+    if (additions?.length > 0)
+    {
+        for(const addition of additions)
+        {
+            // TODO: Figure out if the item not having an ID yet, will be bad for frontend?
+            item[fieldInfo.field].push({ ...addition });
+        }
+    }
+
+    // Handle updates.
+    const updates = changes["update"];
+    if (updates?.length > 0)
+    {
+        for(const update of updates)
+        {
+            // Get the item to update.
+            const updateIndex = item[fieldInfo.field].findIndex((tempItem: any) => tempItem.id === update.id);
+
+            // Update the item value by merging existing values with the updated values.
+            // Even if sort isn't enabled, items won't be rearranged incorrectly.
+            item[fieldInfo.field][updateIndex] = { sort: update?.sort ?? 0, ...item[fieldInfo.field][updateIndex], ...update };
+
+            // const relation = getRelationForField(relations, fieldInfo);
+            // if (relation === undefined) continue;
+            // console.log(relation);
+            // await updateRelatedItemsRecursively(api, relations, relation.collection, item[fieldInfo.field][updateIndex], oldFormValues, newFormValues);
+        }
+
+        // Sort the items in ascending order (1,2,3,4,5).
+        item[fieldInfo.field].sort((itemA: any, itemB: any) => itemA.sort - itemB.sort);
+    }
+
+    // Handle deletes.
+    const deletes = changes["delete"];
+    if (deletes?.length > 0)
+    {
+        for(const deleteId of deletes)
+        {
+            const deleteIndex = item[fieldInfo.field].findIndex((tempItem: any) => tempItem.id === deleteId);
+            item[fieldInfo.field].splice(deleteIndex, 1);
+        }
+    }
+
+    return;
+};
+
+async function handleM2M()
+{
+    return;
+};
+
+async function handleM2A(api: AxiosInstance, item: any, newFormValues: any, fieldInfo: Field)
+{
+    // Get changes.
+    const changes = newFormValues[fieldInfo.field];
+
+    // Handle additions.
+    const additions = changes["create"];
+    if (additions?.length > 0)
+    {
+        for(const addition of additions)
+        {
+            const response = await api.get(`/items/${addition.collection}/${addition.item.id}?fields[]=*.*.*.*.*`);
+            if (response.status !== 200)
+            {
+                console.error(`[Live Preview] Unable to retrieve information about item with id ${addition.item.id} in collection ${addition.collection}`);
+                return "";
+            }
+
+            // Add the new item (__typename is added because I personally use it).
+            item[fieldInfo.field].push({ item: { __typename: addition.collection, ...response.data.data } });
+        }
+    }
+
+    // Handle updates.
+    const updates = changes["update"];
+    if (updates?.length > 0)
+    {
+        for(const update of updates)
+        {
+            // Get the item to update.
+            const updateIndex = item[fieldInfo.field].findIndex((tempInfo: any) => tempInfo.item.id === update.item.id);
+
+            // Update the item value by merging existing values with the updated values.
+            // Even if sort isn't enabled, items won't be rearranged incorrectly.
+            item[fieldInfo.field][updateIndex] = { sort: update?.sort ?? 0, item: { ...item[fieldInfo.field][updateIndex].item, ...update.item } };
+        }
+
+        // Sort the items in ascending order (1,2,3,4,5).
+        item[fieldInfo.field].sort((itemA: any, itemB: any) => itemA.sort - itemB.sort);
+    }
+
+    // Handle deletes.
+    const deletes = changes["delete"];
+    if (deletes?.length > 0)
+    {
+        for(const deleteId of deletes)
+        {
+            // Get relationship information, as we only have the id of the relationship in m2m collection.
+            const response = await api.get(`/items/${fieldInfo.collection}_${fieldInfo.field}/${deleteId}`);
+            if (response.status !== 200)
+            {
+                console.error(`[Live Preview] Unable to retrieve information about item with id ${deleteId} in collection ${fieldInfo.collection}_${fieldInfo.field}`);
+                return "";
+            }
+            
+            // With the collection id, we now find the item to delete.
+            const deleteIndex = item[fieldInfo.field].findIndex((tempItem: any) => tempItem.item.id === response.data.data.item);
+            item[fieldInfo.field].splice(deleteIndex, 1);
+        }
+    }
+
+    return;
+};
--- src/interface.vue
+++ src/interface.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 	import { inject, ref, onMounted, watch } from "vue";
 	import { useApi } from "@directus/extensions-sdk";
+	import { Relation, Field } from "@directus/types";
+	import { getRelations, updateRelatedItemsRecursively } from "./helpers";
 
 	// Properties.
 	interface Properties
@@ -14,8 +16,7 @@
 
 	// Fields.
 	const api = useApi();
-	const values = inject("values");
-	const initialValues = ref<any|undefined>();
+	const values = inject<any>("values");
 	const previewLastUpdated = ref<Date|undefined>();
 
 	// Events.
@@ -24,192 +25,29 @@
 		// Get preview URL from collection.
 		const previewURL = await getPreviewURL();
 
+		// Get relations.
+		const relations = await getRelations(api);
+		if (relations.length === 0) return;
+
 		// Watch changes to form values.
 		watch(values, async (newValue, oldValue) =>
 		{
-			// Get the initial values.
-			if (!initialValues.value) initialValues.value = oldValue;
-
 			if (debugLogs)
 			{
 				console.log("[Live Preview] Values received from form:");
 				console.log(newValue);
 			}
 
-			// Get data of current viewed entity or exit.
-			const entity = await getCollectionEntity(newValue.id);
-			if (!entity) return;
+			// Get current item or exit.
+			const item = await getItem(newValue.id);
+			if (!item) return;
 
-			// Handle changes to related data (refactor this to work recursively, so it can be applied to properties of m2a items).
-			const data = { ...entity["pages_by_id"] };
-			for(const field of Object.keys(data))
-			{
-				// Get value of current field.
-				const value = data[field];
+			// Handle changes to related items (refactor this to work recursively, so it can be applied to properties of m2a items).
+			const data = { ...item["pages_by_id"] };
+			await updateRelatedItemsRecursively(api, relations, collection, data, oldValue, newValue);
 
-				// Get more information about the current field.
-				const fieldTypeResponse = await api.get(`/fields/${collection}`);
-				if (fieldTypeResponse.status !== 200)
-				{
-					console.error(`[Live Preview] Unable to retrieve meta information about field ${field}`);
-					return "";
-				}
-				const fieldInfo = fieldTypeResponse.data.data.find(fieldInfo => fieldInfo.field === field);
-
-				// Simply override field value, if the field doesn't contain relation value.
-				if (!fieldInfo.meta.special?.includes("m2o") && !fieldInfo.meta.special?.includes("o2m") && !fieldInfo.meta.special?.includes("m2a"))
-				{
-					data[field] = newValue[field];
-				}
-				else
-				{
-					if (fieldInfo.meta.special.includes("m2o"))
-					{
-						// Skip, if there are no changes.
-						if (oldValue[field] === newValue[field]) continue;
-
-						// Get foreign collection.
-						const foreignCollection = fieldInfo.schema.foreign_key_table;
-
-						// Get new value from foreign collection.
-						const response = await api.get(`/items/${foreignCollection}/${newValue[field]}?fields[]=*.*.*.*.*`);
-						if (response.status !== 200)
-						{
-							console.error(`[Live Preview] Unable to retrieve information about item with id ${newValue[field]} in foreign collection ${foreignCollection}`);
-							return "";
-						}
-
-						// Update the field value (there will most likely be more properties than originally, but that's ok).
-						data[field] = { ...response.data.data };
-					}
-
-					if (fieldInfo.meta.special.includes("o2m"))
-					{
-						// Get changes.
-						const changes = newValue[field];
-						//TODO: Handle when changes is undefined. This happens when we add a new item and then delete it again.
-
-						// Handle additions.
-						const additions = changes["create"];
-						if (additions?.length > 0)
-						{
-							// Add the new items.
-							for(const addition of additions)
-							{
-								// TODO: Figure out if the item not having an ID yet, will be bad for frontend?
-								data[field].push({ ...addition });
-							}
-						}
-
-						// Handle updates.
-						const updates = changes["update"];
-						if (updates?.length > 0)
-						{
-							for(const update of updates)
-							{
-								// Get the field to update.
-								const updateIndex = data[field].findIndex(info => info.id === update.id);
-
-								// Update the field value by merging existing values with the updated values.
-								// Even if sort isn't enabled, items won't be rearranged incorrectly.
-								data[field][updateIndex] = { sort: update?.sort ?? 0, ...data[field][updateIndex], ...update };
-							}
-
-							// Sort the items in ascending order (1,2,3,4,5).
-							data[field].sort((itemA, itemB) => itemA.sort - itemB.sort);
-						}
-
-						// Handle deletes.
-						const deletes = changes["delete"];
-						if (deletes?.length > 0)
-						{
-							console.log(deletes);
-							// for(const deleteId of deletes)
-							// {
-							// 	// Get relationship information, as we only have the id of the relationship in m2m collection.
-							// 	const response = await api.get(`/items/${collection}_${field}/${deleteId}`);
-							// 	if (response.status !== 200)
-							// 	{
-							// 		console.error(`[Live Preview] Unable to retrieve information about item with id ${deleteId} in collection ${collection}_${field}`);
-							// 		return "";
-							// 	}
-								
-							// 	// With the collection id, we now find the field value to delete.
-							// 	const deleteIndex = data[field].findIndex(info => info.item.id === response.data.data.item);
-
-							// 	// Remove the deleted field value.
-							// 	data[field].splice(deleteIndex, 1);
-							// }
-						}
-					}
-
-					if (fieldInfo.meta.special.includes("m2a"))
-					{
-						// Get changes.
-						const changes = newValue[field];
-
-						// Handle additions.
-						const additions = changes["create"];
-						if (additions?.length > 0)
-						{
-							for(const addition of additions)
-							{
-								const response = await api.get(`/items/${addition.collection}/${addition.item.id}?fields[]=*.*.*.*.*`);
-								if (response.status !== 200)
-								{
-									console.error(`[Live Preview] Unable to retrieve information about item with id ${addition.item.id} in collection ${addition.collection}`);
-									return "";
-								}
-
-								// Add the 
-								data[field].push({ item: { __typename: addition.collection, ...response.data.data } });
-							}
-						}
-
-						// Handle updates.
-						const updates = changes["update"];
-						if (updates?.length > 0)
-						{
-							for(const update of updates)
-							{
-								// Get the field to update.
-								const updateIndex = data[field].findIndex(info => info.item.id === update.item.id);
-
-								// Update the field value by merging existing values with the updated values.
-								data[field][updateIndex] = { sort: update?.sort ?? 0, item: { ...data[field][updateIndex].item, ...update.item } };
-							}
-
-							// Sort the items in ascending order (1,2,3,4,5).
-							data[field].sort((itemA, itemB) => itemA.sort - itemB.sort);
-						}
-
-						// Handle deletes.
-						const deletes = changes["delete"];
-						if (deletes?.length > 0)
-						{
-							for(const deleteId of deletes)
-							{
-								// Get relationship information, as we only have the id of the relationship in m2m collection.
-								const response = await api.get(`/items/${collection}_${field}/${deleteId}`);
-								if (response.status !== 200)
-								{
-									console.error(`[Live Preview] Unable to retrieve information about item with id ${deleteId} in collection ${collection}_${field}`);
-									return "";
-								}
-								
-								// With the collection id, we now find the field value to delete.
-								const deleteIndex = data[field].findIndex(info => info.item.id === response.data.data.item);
-
-								// Remove the deleted field value.
-								data[field].splice(deleteIndex, 1);
-							}
-						}
-					}
-				}
-			}
-
-			// Send entity data to the preview iframe.
-			const previewFrame = document.getElementById("frame")?.contentWindow;
+			// Send item to the preview iframe.
+			const previewFrame = (document.getElementById("frame") as HTMLIFrameElement)?.contentWindow;
 			if (previewFrame)
 			{
 				previewFrame.postMessage({ type: "directus-live-preview", values: data }, previewURL);
@@ -218,14 +56,14 @@
 
 			if (debugLogs)
 			{
-				console.log("[Live Preview] Data sent to preview iframe:");
+				console.log("[Live Preview] Item sent to preview iframe:");
 				console.log(data);
 			}
 		});
 	});
 
 	// Methods.
-	const getPreviewURL = async (): string =>
+	const getPreviewURL = async (): Promise<string> =>
 	{
 		const response = await api.get(`/collections/${collection}`);
 		
@@ -238,14 +76,14 @@
 		return response.data.data.meta.preview_url;
 	};
 
-	const getCollectionEntity = async (id: string): any =>
+	const getItem = async (id: string): Promise<any> =>
 	{
 		const response = requestType === "REST" ? await api.get(`/items/${collection}/${id}${query}`)
 												: await api.post("/graphql", { query, variables: { id: id } });
 
 		if (response.status !== 200)
 		{
-			console.error(`[Live Preview] Unable to retrieve entity with id ${id}`);
+			console.error(`[Live Preview] Unable to retrieve item with id ${id}`);
 			return undefined;
 		}
 
